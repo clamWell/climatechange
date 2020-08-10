7f37e3cfa3e3678b0d69eefc914066e3f2866fae
h62446244! <pepesi258@hanmail.net>
2020-08-10 18:20:35 +0900

    [기후변화기획_인터랙티브] * 퍼블리싱 진행중 * 한국 연평균 기온 편차 추이 막대그래프

--- js/custom.js
+++ js/custom.js
@@ -31,7 +31,216 @@ $(function(){
 		}
 	}
 
+	/******** three.js 글로브 ********/
+	function makeThreeGlobe(){
+
+		var geometry = new THREE.SphereGeometry( 0.5, 32, 32 );
+		var material =new THREE.MeshPhongMaterial({
+			map: THREE.ImageUtils.loadTexture('img/earth-blue-marble-sm.jpg'),
+			//bumpMap: THREE.ImageUtils.loadTexture('img/earth-topology.jpg'),
+			//bumpScale: 0.005,
+		  })
+		var Globe = new THREE.Mesh( geometry, material );
+
+		// Setup renderer
+		const renderer = new THREE.WebGLRenderer();
+		const container = document.getElementById("threeJsGlobe");
+		const { width, height } = container.getBoundingClientRect();
+		renderer.setSize(width, height);
+		container.appendChild(renderer.domElement);
+
+		// Setup scene
+		const scene = new THREE.Scene();
+		scene.background = new THREE.Color("#111");
+		scene.add(Globe);
+
+
+		var amb_light = new THREE.AmbientLight("#ffc66b",1);
+		amb_light.position.set(5,3,5)
+		var dir_light = new THREE.DirectionalLight("#bdf5ff", 0.5);
+		var change_light = new THREE.AmbientLight("#ff2a00", 0);
+		//amb_light.castShadow = true;
+		scene.add(amb_light)
+		scene.add(dir_light)
+		scene.add(change_light)
+
+		// Setup camera
+		const camera = new THREE.PerspectiveCamera();
+		camera.aspect = width/height;
+		camera.updateProjectionMatrix();
+		camera.position.z = 1.8;
+		
+		var it = 0;
+		var dir = 1;
+		// Kick-off renderer
+		(function animate() { // IIFE
+			requestAnimationFrame(animate);
+
+			// Frame cycle
+			//tbControls.update();
+			Globe.rotation.x += 0.001;
+			Globe.rotation.y += 0.002;
+
+			//Globe.rotation.z += 0.01;
+			if(it<1){
+				it += 0.002;
+				change_light.intensity = it;
+			}else if(it>=1){
+
+			}
+
+			renderer.render(scene, camera);
+		})();
 	
+	}
+    makeThreeGlobe();
+	/******** three.js 글로브 ********/
+
+	/******** 도입부 평년 기온차 선 그래프********/
+	function korTempPoleGraph(){
+		var data = koreaTempDevi,
+			width =  ((isMobile==true)? (screenWidth-120) : 900),
+			height = 500,
+			margin = 10,
+			poleMargin = 2;
+			poleWidth = ((width-margin) / data.length)-poleMargin;
+	
+		var devi_val = data.map(function(d) {
+		  return Number(d["deviation"]);
+		});
+		//console.log(devi_val);
+
+		var maxValue = d3.max(devi_val);
+		var minValue = d3.min(devi_val);
+		console.log("편차 max값은: "+ maxValue +" / 편차 min값은: "+minValue);
+
+		var chart_svg = d3.select("#YEAR_DEVI_POLE_CHART");
+		chart_svg.attr("width", width +"px" )
+				.attr("height", height +"px");
+
+		var chart_holder = chart_svg.append("g")
+				.attr("class","chart-holder");
+
+		var x = d3.scaleTime()
+				.range([0, width])
+				.domain([new Date(1973, 0, 1), new Date(2019, 0, 1)])
+
+		var y = d3.scaleLinear()
+				.range([height, 0])
+				.domain([minValue, maxValue])
+			
+		chart_holder.append("g")
+		  .attr("class", "y axis")
+		  .attr("transform", "translate(-10,0)")
+		  .call(d3.axisLeft(y).ticks(5));
+
+		chart_holder.append("g")
+		  .attr("class", "x axis")
+		  .attr("transform", "translate(0,"+ (height+20)+")")
+		  .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%Y")).ticks(4));
+
+		var pole_holder = chart_holder.append("g")
+		  .attr("class", "pole-holder")
+		  .attr("transform", "translate(0,"+ -280 +")")
+
+		var pole_year_avr = pole_holder.selectAll("g")
+				.data(data)
+				.enter().append("g")
+				.attr("class","pole-g")
+				.attr("transform", function(d, i) {
+					var transX = ( i * (poleWidth  + poleMargin) );
+					return "translate("+ transX +",0)";}
+				 );
+
+		var getPoleColor = function(t){
+			if (t<=0){
+				//return "#6bb8d8";
+				return "url(#coldGrad)";
+			}else{
+				//return "#ff8a15";
+				return "url(#warmGrad)";
+			}
+		};
+		
+		var minMaxSum = Number(maxValue) + Number(Math.abs(minValue)); 
+		var multipleKey =  height/minMaxSum;
+
+		pole_year_avr.append("rect")
+				.style("fill", function(d) {
+					return getPoleColor(d["deviation"]);
+				})
+				.attr("width", poleWidth)
+				.attr("class", "pole pole-each-year-avr")
+				.attr("height", function(d){
+					var h = Math.abs( Number(d["deviation"]) ) * multipleKey;
+					if(h<4){ h = 4;}
+					return h;
+				}).attr("x", function(d, i) {
+					return 0;
+				}).attr("y", function(d) {
+					if(Number(d["deviation"]) <= 0){
+						return height;
+					}else{
+						var h = Math.abs( Number(d["deviation"]) ) * multipleKey;
+						if(h<4){ h = 4;}
+						return height-h;
+					}
+				});
+			
+		pole_year_avr.append("text")
+				.attr("class","pole-label")
+				.text(function(d) { return Number(d["deviation"]).toFixed(2) +"℃"; })
+				.attr("transform", function(d, i) { 
+					var labelTransY;
+					var h = Math.abs( Number(d["deviation"]) ) * multipleKey;
+					if(Number(d["deviation"]) <= 0){
+						labelTransY = height + h + 5;;
+					}else{
+						if(h<4){ h = 4;}
+						labelTransY = height-h -5;
+					}
+					return "translate(5," +labelTransY+")";
+				})
+
+		var $tooltip = $("#TOOLTIP_YEAR_DEVI");
+		$tooltip.css({"opacity":"0"});
+		pole_year_avr.on("mouseenter", function(d) {
+					d3.selectAll("#YEAR_DEVI_POLE_CHART .pole").style("fill-opacity", "0.4")
+					d3.select(this).selectAll(".pole")
+						.style("fill-opacity", "1")
+						.style("stroke", "#333")
+
+					d3.select(this).selectAll(".pole-label")
+						.style("fill-opacity", "1")
+
+					$tooltip.find(".year span").html(d["year"]);
+					$tooltip.find(".temp_avr .value").html(d["temp_avr"]);
+					$tooltip.find(".temp_devi .value").html(d["deviation"]);
+					$tooltip.find(".temp_devi .value").removeClass("value-cold value-warm");
+					if(d["deviation"]<0){
+						$tooltip.find(".temp_devi .value").addClass("value-cold");
+					}else if(d["deviation"]>=0){
+						$tooltip.find(".temp_devi .value").addClass("value-warm");
+					}
+					$tooltip.css({"left":(d3.mouse(this.parentNode)[0])+"px"});
+					$tooltip.css({"bottom":"-20px"});
+					$tooltip.css({"opacity":"1"})
+
+				}).on("mouseleave", function(d){
+					d3.selectAll("#YEAR_DEVI_POLE_CHART .pole")
+						.style("fill-opacity", null)
+						.style("stroke", null)
+						.style("stroke-width",  null)
+					$tooltip.css({"opacity":"0"})
+					d3.selectAll("#YEAR_DEVI_POLE_CHART .pole-label")
+						.style("fill-opacity", null)
+				});		
+
+
+	}	
+	korTempPoleGraph();
+
+	/******** 도입부 평년 기온차 선 그래프********/
 
 	/********progress********/
 	var progressBar = {
@@ -68,92 +277,16 @@ $(function(){
 		
 	}
 	/******** 모바일 전용 조정 ********/
-
+	var showHeader = function() {
+		$(".common_header").delay(500).animate({"top": "0px"}, 1000, "swing");
+	}
 	function init(){
-	
+		showHeader();
 	}
 
 	$(".loading-page").fadeOut(200, function(){
 		init();
 	});
-
-
-	/******** three.js 글로브 ********/
-	function makeThreeGlobe(){
-		/*
-		 const Globe = new ThreeGlobe()
-		.globeImageUrl('https://unpkg.com/three-globe@2.8.7/example/img/earth-blue-marble.jpg')
-		.bumpImageUrl('https://unpkg.com/three-globe@2.8.7/example/img/earth-topology.png')
-		*/
-	
-		var geometry = new THREE.SphereGeometry( 0.5, 32, 32 );
-		var material =new THREE.MeshPhongMaterial({
-			map: THREE.ImageUtils.loadTexture('img/earth-blue-marble-sm.jpg'),
-			//bumpMap: THREE.ImageUtils.loadTexture('img/earth-topology.jpg'),
-			//bumpScale: 0.005,
-		  })
-		var Globe = new THREE.Mesh( geometry, material );
-
-		// Setup renderer
-		const renderer = new THREE.WebGLRenderer();
-		const container = document.getElementById("threeJsGlobe");
-		const { width, height } = container.getBoundingClientRect();
-		renderer.setSize(width, height);
-		container.appendChild(renderer.domElement);
-
-		// Setup scene
-		const scene = new THREE.Scene();
-		scene.background = new THREE.Color("#111");
-		scene.add(Globe);
-
-
-		var amb_light = new THREE.AmbientLight("#ffc66b",1);
-		amb_light.position.set(5,3,5)
-		var dir_light = new THREE.DirectionalLight("#bdf5ff", 0.5);
-		var change_light = new THREE.AmbientLight("#ff2a00", 0);
-		//amb_light.castShadow = true;
-		scene.add(amb_light)
-		scene.add(dir_light)
-		scene.add(change_light)
-
-		// Setup camera
-		const camera = new THREE.PerspectiveCamera();
-		camera.aspect = width/height;
-		camera.updateProjectionMatrix();
-		camera.position.z = 2;
-
-		// Add camera controls
-		/*
-		const tbControls = new THREE.TrackballControls(camera, renderer.domElement);
-		tbControls.minDistance = 101;
-		tbControls.rotateSpeed = 5;
-		tbControls.zoomSpeed = 0.8;*/
-		
-		var it = 0;
-		var dir = 1;
-		// Kick-off renderer
-		(function animate() { // IIFE
-			requestAnimationFrame(animate);
-
-			// Frame cycle
-			//tbControls.update();
-			Globe.rotation.x += 0.001;
-			Globe.rotation.y += 0.002;
-
-			//Globe.rotation.z += 0.01;
-			if(it<1){
-				it += 0.002;
-				change_light.intensity = it;
-			}else if(it>=1){
-
-			}
-
-			renderer.render(scene, camera);
-		})();
-	
-	}
-    makeThreeGlobe();
-	/******** three.js 글로브 ********/
 
 
 	$(window).scroll(function(){
---
+++ js/korTempData.js
@@ -0,0 +1,425 @@
+var koreaTempDevi = [
+  {
+    "year": "1973",
+    "temp_avr": "12.4",
+    "temp_spr": "11.6",
+    "temp_sum": "24.5",
+    "temp_au": "12.9",
+    "temp_win": "-1.4",
+    "deviation": "-0.1362"
+  },
+  {
+    "year": "1974",
+    "temp_avr": "11.4",
+    "temp_spr": "10.8",
+    "temp_sum": "22.4",
+    "temp_au": "13",
+    "temp_win": "-0.1",
+    "deviation": "-1.1362"
+  },
+  {
+    "year": "1975",
+    "temp_avr": "12.6",
+    "temp_spr": "11.2",
+    "temp_sum": "23.9",
+    "temp_au": "15.5",
+    "temp_win": "0.3",
+    "deviation": "0.0638"
+  },
+  {
+    "year": "1976",
+    "temp_avr": "11.7",
+    "temp_spr": "10.9",
+    "temp_sum": "22.6",
+    "temp_au": "12.5",
+    "temp_win": "-1.7",
+    "deviation": "-0.8362"
+  },
+  {
+    "year": "1977",
+    "temp_avr": "12.3",
+    "temp_spr": "11.8",
+    "temp_sum": "23.5",
+    "temp_au": "15",
+    "temp_win": "0.7",
+    "deviation": "-0.2362"
+  },
+  {
+    "year": "1978",
+    "temp_avr": "12.8",
+    "temp_spr": "11.7",
+    "temp_sum": "24.7",
+    "temp_au": "14.3",
+    "temp_win": "2.2",
+    "deviation": "0.2638"
+  },
+  {
+    "year": "1979",
+    "temp_avr": "12.6",
+    "temp_spr": "11.2",
+    "temp_sum": "23.5",
+    "temp_au": "13.6",
+    "temp_win": "-0.2",
+    "deviation": "0.0638"
+  },
+  {
+    "year": "1980",
+    "temp_avr": "11.2",
+    "temp_spr": "11",
+    "temp_sum": "22.1",
+    "temp_au": "13.4",
+    "temp_win": "-2.3",
+    "deviation": "-1.3362"
+  },
+  {
+    "year": "1981",
+    "temp_avr": "11.4",
+    "temp_spr": "11.5",
+    "temp_sum": "23.6",
+    "temp_au": "12.2",
+    "temp_win": "-0.1",
+    "deviation": "-1.1362"
+  },
+  {
+    "year": "1982",
+    "temp_avr": "12.4",
+    "temp_spr": "12",
+    "temp_sum": "23.4",
+    "temp_au": "14.3",
+    "temp_win": "-0.2",
+    "deviation": "-0.1362"
+  },
+  {
+    "year": "1983",
+    "temp_avr": "12.3",
+    "temp_spr": "12.2",
+    "temp_sum": "23.4",
+    "temp_au": "14.2",
+    "temp_win": "-2.1",
+    "deviation": "-0.2362"
+  },
+  {
+    "year": "1984",
+    "temp_avr": "11.8",
+    "temp_spr": "10.7",
+    "temp_sum": "24.3",
+    "temp_au": "13.8",
+    "temp_win": "-0.6",
+    "deviation": "-0.7362"
+  },
+  {
+    "year": "1985",
+    "temp_avr": "12.1",
+    "temp_spr": "11.5",
+    "temp_sum": "24.1",
+    "temp_au": "14.3",
+    "temp_win": "-2.1",
+    "deviation": "-0.4362"
+  },
+  {
+    "year": "1986",
+    "temp_avr": "11.5",
+    "temp_spr": "11.4",
+    "temp_sum": "22.9",
+    "temp_au": "12.6",
+    "temp_win": "1.2",
+    "deviation": "-1.0362"
+  },
+  {
+    "year": "1987",
+    "temp_avr": "12.2",
+    "temp_spr": "11.1",
+    "temp_sum": "23.2",
+    "temp_au": "14.1",
+    "temp_win": "0.3",
+    "deviation": "-0.3362"
+  },
+  {
+    "year": "1988",
+    "temp_avr": "12.1",
+    "temp_spr": "11",
+    "temp_sum": "23.6",
+    "temp_au": "13.6",
+    "temp_win": "1.7",
+    "deviation": "-0.4362"
+  },
+  {
+    "year": "1989",
+    "temp_avr": "12.8",
+    "temp_spr": "12.3",
+    "temp_sum": "22.9",
+    "temp_au": "13.7",
+    "temp_win": "1.4",
+    "deviation": "0.2638"
+  },
+  {
+    "year": "1990",
+    "temp_avr": "13.1",
+    "temp_spr": "11.7",
+    "temp_sum": "24.3",
+    "temp_au": "15.2",
+    "temp_win": "0.2",
+    "deviation": "0.5638"
+  },
+  {
+    "year": "1991",
+    "temp_avr": "12.2",
+    "temp_spr": "11.4",
+    "temp_sum": "23.4",
+    "temp_au": "13.5",
+    "temp_win": "1.7",
+    "deviation": "-0.3362"
+  },
+  {
+    "year": "1992",
+    "temp_avr": "12.4",
+    "temp_spr": "11.6",
+    "temp_sum": "23.2",
+    "temp_au": "13.4",
+    "temp_win": "1.1",
+    "deviation": "-0.1362"
+  },
+  {
+    "year": "1993",
+    "temp_avr": "11.8",
+    "temp_spr": "11.1",
+    "temp_sum": "21.7",
+    "temp_au": "13.8",
+    "temp_win": "0.5",
+    "deviation": "-0.7362"
+  },
+  {
+    "year": "1994",
+    "temp_avr": "13.2",
+    "temp_spr": "11.8",
+    "temp_sum": "25.3",
+    "temp_au": "14.8",
+    "temp_win": "0.8",
+    "deviation": "0.6638"
+  },
+  {
+    "year": "1995",
+    "temp_avr": "12",
+    "temp_spr": "11.1",
+    "temp_sum": "23.7",
+    "temp_au": "13.4",
+    "temp_win": "-0.6",
+    "deviation": "-0.5362"
+  },
+  {
+    "year": "1996",
+    "temp_avr": "12.1",
+    "temp_spr": "10.6",
+    "temp_sum": "23.5",
+    "temp_au": "14.1",
+    "temp_win": "0.5",
+    "deviation": "-0.4362"
+  },
+  {
+    "year": "1997",
+    "temp_avr": "12.7",
+    "temp_spr": "12.1",
+    "temp_sum": "24",
+    "temp_au": "14",
+    "temp_win": "1.9",
+    "deviation": "0.1638"
+  },
+  {
+    "year": "1998",
+    "temp_avr": "13.5",
+    "temp_spr": "13.4",
+    "temp_sum": "23.1",
+    "temp_au": "15.4",
+    "temp_win": "1.6",
+    "deviation": "0.9638"
+  },
+  {
+    "year": "1999",
+    "temp_avr": "12.8",
+    "temp_spr": "12.1",
+    "temp_sum": "23.3",
+    "temp_au": "14.7",
+    "temp_win": "0.3",
+    "deviation": "0.2638"
+  },
+  {
+    "year": "2000",
+    "temp_avr": "12.5",
+    "temp_spr": "11.6",
+    "temp_sum": "24.2",
+    "temp_au": "13.7",
+    "temp_win": "0.3",
+    "deviation": "-0.0362"
+  },
+  {
+    "year": "2001",
+    "temp_avr": "12.7",
+    "temp_spr": "12.2",
+    "temp_sum": "24.2",
+    "temp_au": "14.3",
+    "temp_win": "1.5",
+    "deviation": "0.1638"
+  },
+  {
+    "year": "2002",
+    "temp_avr": "12.6",
+    "temp_spr": "12.7",
+    "temp_sum": "23.1",
+    "temp_au": "12.5",
+    "temp_win": "0.9",
+    "deviation": "0.0638"
+  },
+  {
+    "year": "2003",
+    "temp_avr": "12.5",
+    "temp_spr": "11.9",
+    "temp_sum": "22.3",
+    "temp_au": "14.7",
+    "temp_win": "1.4",
+    "deviation": "-0.0362"
+  },
+  {
+    "year": "2004",
+    "temp_avr": "13.2",
+    "temp_spr": "12.2",
+    "temp_sum": "24",
+    "temp_au": "14.7",
+    "temp_win": "0.5",
+    "deviation": "0.6638"
+  },
+  {
+    "year": "2005",
+    "temp_avr": "12.3",
+    "temp_spr": "11.7",
+    "temp_sum": "24.1",
+    "temp_au": "14.8",
+    "temp_win": "-0.1",
+    "deviation": "-0.2362"
+  },
+  {
+    "year": "2006",
+    "temp_avr": "12.9",
+    "temp_spr": "11.5",
+    "temp_sum": "23.6",
+    "temp_au": "15.1",
+    "temp_win": "2.4",
+    "deviation": "0.3638"
+  },
+  {
+    "year": "2007",
+    "temp_avr": "13.2",
+    "temp_spr": "12.1",
+    "temp_sum": "23.8",
+    "temp_au": "14.5",
+    "temp_win": "0.7",
+    "deviation": "0.6638"
+  },
+  {
+    "year": "2008",
+    "temp_avr": "12.9",
+    "temp_spr": "12.5",
+    "temp_sum": "23.7",
+    "temp_au": "15.1",
+    "temp_win": "1.7",
+    "deviation": "0.3638"
+  },
+  {
+    "year": "2009",
+    "temp_avr": "13",
+    "temp_spr": "12.6",
+    "temp_sum": "23.3",
+    "temp_au": "14.7",
+    "temp_win": "0.5",
+    "deviation": "0.4638"
+  },
+  {
+    "year": "2010",
+    "temp_avr": "12.7",
+    "temp_spr": "10.8",
+    "temp_sum": "24.9",
+    "temp_au": "14.5",
+    "temp_win": "-0.7",
+    "deviation": "0.1638"
+  },
+  {
+    "year": "2011",
+    "temp_avr": "12.4",
+    "temp_spr": "11",
+    "temp_sum": "24",
+    "temp_au": "15.3",
+    "temp_win": "-0.4",
+    "deviation": "-0.1362"
+  },
+  {
+    "year": "2012",
+    "temp_avr": "12.3",
+    "temp_spr": "12.2",
+    "temp_sum": "24.7",
+    "temp_au": "13.7",
+    "temp_win": "-1",
+    "deviation": "-0.2362"
+  },
+  {
+    "year": "2013",
+    "temp_avr": "12.9",
+    "temp_spr": "11.6",
+    "temp_sum": "25.4",
+    "temp_au": "14.6",
+    "temp_win": "1.5",
+    "deviation": "0.3638"
+  },
+  {
+    "year": "2014",
+    "temp_avr": "13.1",
+    "temp_spr": "13.1",
+    "temp_sum": "23.6",
+    "temp_au": "14.9",
+    "temp_win": "0.7",
+    "deviation": "0.5638"
+  },
+  {
+    "year": "2015",
+    "temp_avr": "13.4",
+    "temp_spr": "12.7",
+    "temp_sum": "23.7",
+    "temp_au": "15.2",
+    "temp_win": "1.4",
+    "deviation": "0.8638"
+  },
+  {
+    "year": "2016",
+    "temp_avr": "13.6",
+    "temp_spr": "13.2",
+    "temp_sum": "24.8",
+    "temp_au": "15.1",
+    "temp_win": "1.6",
+    "deviation": "1.0638"
+  },
+  {
+    "year": "2017",
+    "temp_avr": "13.1",
+    "temp_spr": "13",
+    "temp_sum": "24.5",
+    "temp_au": "14.2",
+    "temp_win": "-0.8",
+    "deviation": "0.5638"
+  },
+  {
+    "year": "2018",
+    "temp_avr": "13",
+    "temp_spr": "13.1",
+    "temp_sum": "25.4",
+    "temp_au": "13.8",
+    "temp_win": "1.3",
+    "deviation": "0.4638"
+  },
+  {
+    "year": "2019",
+    "temp_avr": "13.5",
+    "temp_spr": "12.7",
+    "temp_sum": "24.1",
+    "temp_au": "15.4",
+    "temp_win": "3.1",
+    "deviation": "0.9638"
+  }
+];
